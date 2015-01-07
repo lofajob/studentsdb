15aa811e2129f3f3bd13f3c440ffd8512f9cf1cb
Oleksiy <lofa_job@ukr.net>
2015-01-07 20:05:47 +0200

    create Students database

--- studentdb/settings.py
+++ studentdb/settings.py
@@ -15,10 +15,11 @@ from django.conf import global_settings
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 
 #old_one
 #PORTAL_URL = 'http://localhost:8000'
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
